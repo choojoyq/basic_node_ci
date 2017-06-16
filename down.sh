99bc1378612fc732cee585dceafa2764cfaacfa3
Radziuk Aliaksandr <aliaksandr.radziuk@altoros.com>
2017-06-16 18:47:26 +0300

    just commit

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-docker-compose down
-docker rm node_db
+docker-compose stop web
+#docker rm node_db
 docker rm node_web
 docker rmi -f basicnodeci_web
-docker rmi -f basicnodeci_db
+#docker rmi -f basicnodeci_db
